c87e4715fad25c0b947f73c9cb5ca2eab116b0fa
WireNut88 <115939048+WireNut88@users.noreply.github.com>
2024-10-05 16:13:52 -0500

    This adds a little to database and adds template section along with basic streak template. Needs to be moved to homepage in future

--- cs4090/cs4090/urls.py
+++ cs4090/cs4090/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('webApp.urls')) # include urls from webApp urls here
+    path('',include('webApp.urls')), # include urls from webApp urls here
+    
 ]
