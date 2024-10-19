77f42f89fd26ddf1ac5cb0587b76be979b61d248
WireNut88 <115939048+WireNut88@users.noreply.github.com>
2024-10-19 17:35:19 -0500

    the gamer UI

--- cs4090/cs4090/settings.py
+++ cs4090/cs4090/settings.py
@@ -121,11 +121,27 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+"""
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+"""
 
-STATIC_URL = "static/"
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+#STATIC_ROOT=os.path.join(BASE_DIR, 'static/')
+
+#
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/') # 'data' is my media folder
+MEDIA_URL = '/media/'
+
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
