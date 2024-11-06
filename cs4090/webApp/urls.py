8818c0208edd4309d5faf24cb2a3fa658b04d039
WireNut88 <115939048+WireNut88@users.noreply.github.com>
2024-11-06 10:32:15 -0600

    added a profile page, it needs UI improvement but has a way to update canvas api key

--- cs4090/webApp/urls.py
+++ cs4090/webApp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import index, streak_view
+from .views import index, streak_view, profile_view
 
 urlpatterns = [  # standard method for keeping track of urls.
     path(
@@ -8,4 +8,5 @@ urlpatterns = [  # standard method for keeping track of urls.
     ),  # arguments- urlPath, what happens when u visit that url ex what function will run, give it a name
     # path('calendar/', views.index1.as_view(), name='calendar'),
     path("streak/", streak_view, name="streak_view"),
+    path("profile/", profile_view, name="profile"),
 ]
